b13a039e7a1f16b345f3f3ac260632cf90177970
andreylavrusenko <m>
2023-06-15 19:57:05 +0300

    add reg page

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,11 +1,14 @@
 import '../style/components/navbar.scss'
+import {NavLink} from "react-router-dom";
 
 export const NavBar = () => {
 	return (
 		<div className="nav__footer">
-			<div className="nav__footer-item">1</div>
-			<div className="nav__footer-item">2</div>
-			<div className="nav__footer-item">3</div>
+			<NavLink to="/" className="nav__footer-item">Тренировка</NavLink>
+			<div className="nav__footer-divider"></div>
+			<div className="nav__footer-item">Создать</div>
+			<div className="nav__footer-divider"></div>
+			<NavLink to="/login" className="nav__footer-item">Логин</NavLink>
 		</div>
 	)
 }
